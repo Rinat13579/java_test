b5fb45fdb5652572daed8219e28a73da0f83fa8d
Rinat13579 <zakirrzgmail.com>
2020-04-26 15:36:15 +0300

    learned functions

--- sandbox/src/main/java/ru/jt/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/jt/sandbox/FirstProgram.java
@@ -2,6 +2,27 @@ package ru.jt.sandbox;
 
 public class FirstProgram {
 	public static void main (String[] args) {
-		System.out.println("Hello, World!");
+		hello("World");
+		hello("User");
+		hello("Name");
+
+		double l = 5;
+		System.out.println("Square area with side " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Square area with sides " + l +" and " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody){
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double len){
+		return len*len;
+	}
+
+	public static double area (double a, double b){
+		return a*b;
 	}
 }
