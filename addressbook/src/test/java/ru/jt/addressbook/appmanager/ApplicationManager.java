b1f69c3aa217fcfb30348b9ef9dbb1ae585adc5a
Rinat13579 <zakirrzgmail.com>
2020-05-03 16:31:03 +0300

    refactoring

--- addressbook/src/test/java/ru/jt/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/jt/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,8 @@
 package ru.jt.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
-import static org.testng.Assert.fail;
 
 public class ApplicationManager {
 
@@ -11,7 +10,7 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
-    private boolean acceptNextAlert = true;
+
     private StringBuffer verificationErrors;
 
     public void init() {
@@ -24,43 +23,8 @@ public class ApplicationManager {
         sessionHelper.login("admin", "secret");
     }
 
-
-
     public void stop() {
         driver.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-      try {
-        driver.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
-
-    private boolean isAlertPresent() {
-      try {
-        driver.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
-
-    private String closeAlertAndGetItsText() {
-      try {
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        if (acceptNextAlert) {
-          alert.accept();
-        } else {
-          alert.dismiss();
-        }
-        return alertText;
-      } finally {
-        acceptNextAlert = true;
-      }
     }
 
     public GroupHelper getGroupHelper() {
